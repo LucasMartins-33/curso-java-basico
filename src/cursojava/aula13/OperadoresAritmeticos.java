1dc07bc438ec2fcc1bacbb93edfa91e20893f6b4
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2024-11-26 21:49:12 -0300

    aula 13 sobre operadores

---
+++ src/cursojava/aula13/OperadoresAritmeticos.java
@@ -0,0 +1,24 @@
+package cursojava.aula13;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int resultado = 1 + 2;
+        System.out.println(resultado);
+
+        resultado -= 1;
+        System.out.println(resultado);
+
+        resultado *= 2;
+        System.out.println(resultado);
+
+        resultado /= 2;
+        System.out.println(resultado);
+
+        resultado += 8;
+        System.out.println(resultado);
+
+        resultado %= 7;
+        System.out.println(resultado);
+
+    }
+}
