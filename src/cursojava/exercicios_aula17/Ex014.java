0b4c406118ea195c5c0ccabc3069ac44c86e0cea
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-01-15 17:52:39 -0300

    Exercícios 013 e 014 atualizados

---
+++ src/cursojava/exercicios_aula17/Ex014.java
@@ -0,0 +1,23 @@
+package cursojava.exercicios_aula17;
+import java.util.Scanner;
+public class Ex014 {
+    public static void main(String[] arg){
+        Scanner scan = new Scanner(System.in);
+        int num;
+        int par = 0;
+        int impar = 0;
+        for(int cont = 0; cont < 10; cont++) {
+            System.out.println("Digite um número: ");
+            num = scan.nextInt();
+
+            if (num % 2 == 0){
+                par += 1;
+            }
+            else{
+                impar += 1;
+            }
+        }
+        System.out.printf("Quantidade de números pares: %d\n", par);
+        System.out.printf("Quantidade de números ímpares: %d", impar);
+    }
+}
