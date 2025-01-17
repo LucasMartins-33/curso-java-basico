e9cfbba7b9feaaf544723fd6d49aff306b599739
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-01-17 13:04:25 -0300

    Exercício 016 atualizado

---
+++ src/cursojava/exercicios_aula17/Ex016.java
@@ -0,0 +1,21 @@
+package cursojava.exercicios_aula17;
+
+public class Ex016 {
+    public static void main(String[] arg){
+
+        int primeiro = 1;
+        int segundo = 1;
+        int proximo = 0;
+
+        System.out.println(primeiro);
+        System.out.println(segundo);
+
+        while (proximo <= 500){
+            proximo = primeiro + segundo;
+            primeiro = segundo;
+            segundo = proximo;
+            System.out.println(proximo);
+
+        }
+    }
+}
