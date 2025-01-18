13f0b69526d8f4b3e0b7bb1f28e116ba7234af18
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-01-18 13:53:02 -0300

    Exercício024 atualizado

---
+++ src/cursojava/exercicios_aula17/Ex024.java
@@ -0,0 +1,22 @@
+package cursojava.exercicios_aula17;
+import java.util.Scanner;
+public class Ex024 {
+    public static void main(String[] arg){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Quantas notas você quer calcular a média: ");
+        int num = scan.nextInt();
+
+        double tot = 0;
+        double media = 0;
+
+        for (int cont = 1; cont <= num; cont++) {
+
+            System.out.printf("Infome as notas N%d: \n", cont);
+            double notas = scan.nextDouble();
+            tot += notas;
+        }
+        media = tot / num;
+        System.out.printf("A média foi de %.1f", media);
+    }
+}
