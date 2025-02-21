00097f351e6896a2aea6967c8ded30f5f3e66bfa
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-02-21 15:40:06 -0300

    Atualização do exercício 038
    Adição de um formato decimal  na linha 10 e algumas mudanças nas variáveis salário e a remoção da variável novoSalario

--- src/cursojava/exercicios_aula17/Ex038.java
+++ src/cursojava/exercicios_aula17/Ex038.java
@@ -1,22 +1,21 @@
 package cursojava.exercicios_aula17;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 public class Ex038 {
     public static void main(String[] arg) {
         double salario = 1000; //95
-        double percentual = 0.015; //96
-        double novoSalario = salario + (salario * percentual); //96
+        double percentual = 1.5; //96
+        salario += (salario/100)  * percentual; //96
 
+        DecimalFormat format = new DecimalFormat("###,###.##");
         for (int cont = 1997; cont <= 2015; cont++) {
             percentual *= 2;
 
-            novoSalario = salario + (salario * percentual);
+            salario += (salario/100)  * percentual;
 
-            System.out.printf("%d = R$ %.2f com percentual de %.2f \n", cont, novoSalario, percentual);
+            System.out.println(cont + " = " + format.format(salario) + " - " + percentual + " % ");
 
-
+            //System.out.printf("%d = R$ %.2f com percentual de %.2f \n", cont, format.format(novoSalario), percentual);
         }
-
-
-        System.out.println(novoSalario);
     }
 }
