98cb2b2df5ef43612a0dd3a94534bd1a7aa0b21f
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-04-14 15:03:06 -0300

    Ex006 atualizado

---
+++ src/cursojava/exercicios_aula19/Ex006.java
@@ -0,0 +1,36 @@
+package cursojava.exercicios_aula19;
+import java.util.Scanner;
+public class Ex006 {
+    public static void main(String[] agr){
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[3];
+        int[] vetorB = new int[vetorA.length];
+        int[] vetorC = new int[vetorA.length];
+
+        for (int i = 0, j = 0; i < vetorA.length && j < vetorB.length; i++, j++){
+            System.out.printf("Insira o valor no vetor A, na posição %d: ",i);
+            vetorA[i] = scan.nextInt();
+
+            System.out.printf("Insira o valor no vetor B, na posição %d: ",j);
+            vetorB[i] = scan.nextInt();
+
+            vetorC[i] = vetorA[i] + vetorB[i];
+        }
+
+        System.out.print("Vetor A = ");
+        for (int valorA : vetorA){
+            System.out.print(valorA + " ");
+        }
+
+        System.out.print("Vetor B = ");
+        for (int valorB : vetorB){
+            System.out.print(valorB + " ");
+        }
+
+        System.out.print("Vetor C = ");
+        for (int valorC : vetorC){
+            System.out.print(valorC + " ");
+        }
+    }
+}
