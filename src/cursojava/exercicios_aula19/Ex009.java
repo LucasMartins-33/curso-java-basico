45589af30002a0b7e1098e8bb6c4c21bf83b80cb
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-04-14 20:42:44 -0300

    Ex009 atualizado

---
+++ src/cursojava/exercicios_aula19/Ex009.java
@@ -0,0 +1,43 @@
+package cursojava.exercicios_aula19;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class Ex009 {
+    public static void main(String[] arg){
+        Scanner scan = new Scanner(System.in);
+
+        double[] vetorA = new double[3];
+        double[] vetorB = new double[vetorA.length];
+        double[] vetorC = new double[vetorA.length];
+
+        for (int i = 0; i < vetorA.length; i++){
+            System.out.printf("Insira o valor no vetor A, na posição %d: ",i);
+            vetorA[i] = scan.nextDouble();
+
+            System.out.printf("Insira o valor no vetor B, na posição %d: ",i);
+            vetorB[i] = scan.nextDouble();
+
+            vetorC[i] = vetorA[i] / vetorB[i];
+        }
+
+        System.out.print("Vetor A = ");
+        for (double valorA : vetorA){
+            System.out.print(valorA + " ");
+        }
+
+        System.out.println();
+
+        System.out.print("Vetor B = ");
+        for (double valorB : vetorB){
+            System.out.print(valorB + " ");
+        }
+
+        System.out.println();
+
+        DecimalFormat df = new DecimalFormat("###,###.##");
+
+        System.out.print("Vetor C = ");
+        for (double valorC : vetorC){
+            System.out.print(df.format(valorC) + " ");
+        }
+    }
+}
