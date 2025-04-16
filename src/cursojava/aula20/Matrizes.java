4cfc60cc2230ffaecf83214b87749cbbd28ce944
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-04-16 12:36:43 -0300

    Aula020 sobre matrizes

---
+++ src/cursojava/aula20/Matrizes.java
@@ -0,0 +1,19 @@
+package cursojava.aula20;
+
+public class Matrizes {
+    public static void main(String[] arg){
+
+        double[][] notasAlunos = new double[30][4]; // A ordem não importa
+
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 7;
+        notasAlunos[0][2] = 9;
+        notasAlunos[0][3] = 9.5;
+
+        notasAlunos[1][0] = 9;
+        notasAlunos[1][1] = 7;
+        notasAlunos[1][2] = 8;
+        notasAlunos[1][3] = 9.5;
+
+    }
+}
