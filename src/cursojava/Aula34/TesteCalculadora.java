9b26f926c9e025e36201cea9b20490f5b66b4223
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-06-02 19:30:29 -0300

    excalc

---
+++ src/cursojava/Aula34/TesteCalculadora.java
@@ -0,0 +1,12 @@
+package cursojava.Aula34;
+
+public class TesteCalculadora {
+    public static void main(String[] arg) {
+
+        MinhaCalculadora calc = new MinhaCalculadora();
+
+        calc.soma(1, 2);
+
+
+    }
+}
