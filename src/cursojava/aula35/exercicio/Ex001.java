6e4b573492fa6d89c62f185bf6439b6fa9644e42
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-06-03 16:17:42 -0300

    Ex004 sobre fibonacci

---
+++ src/cursojava/aula35/exercicio/Ex001.java
@@ -0,0 +1,11 @@
+package cursojava.aula35.exercicio;
+
+public class Ex001 {
+    public static void main(String[] arg){
+
+        for (int i = 1; i < 10; i++){
+            System.out.println(Calculadora.fibonacci(i) + " ");
+        }
+
+    }
+}
