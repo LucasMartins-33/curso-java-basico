44c493db075a332387aaa1a95df860950ff2fd3e
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-06-02 22:07:30 -0300

    Atualização da aula sobre static

--- src/cursojava/Aula34/TesteCalculadora.java
+++ src/cursojava/Aula34/TesteCalculadora.java
@@ -1,12 +1,22 @@
 package cursojava.Aula34;
 
 public class TesteCalculadora {
+
+    static int resultSoma;
     public static void main(String[] arg) {
 
-        MinhaCalculadora calc = new MinhaCalculadora();
+        resultSoma = MinhaCalculadora.soma(1,2,3);
 
-        calc.soma(1, 2);
+        //MinhaCalculadora calc = new MinhaCalculadora();
+        //calc.soma(1, 2);
+
+        soma2Valores(1, 2);
 
 
+
+
+    }
+    static int soma2Valores(int num1, int num2){
+        return MinhaCalculadora.soma(num1, num2);
     }
 }
