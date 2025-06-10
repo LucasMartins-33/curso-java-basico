f2c0bf08ec088a887d4935acea5db9cec3127c17
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-06-10 15:53:57 -0300

    aula36 sobre tem-um e tem-muitos

---
+++ src/cursojava/aula036/Endereco.java
@@ -0,0 +1,59 @@
+package cursojava.aula036;
+
+public class Endereco {
+
+    private String nomeRua;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    public String getNomeRua() {
+        return nomeRua;
+    }
+
+    public void setNomeRua(String nomeRua) {
+        this.nomeRua = nomeRua;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
