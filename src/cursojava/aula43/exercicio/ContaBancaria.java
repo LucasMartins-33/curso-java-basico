062cc28c53619d2d19e609d4b5344f44e919c6fc
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-07-02 15:46:52 -0300

    Ex001 - Conta Bancaria
    Criação da classe ContaBancaria, adição de atributos, métodos get e set, construtores e métodos sacar e depositar

---
+++ src/cursojava/aula43/exercicio/ContaBancaria.java
@@ -0,0 +1,68 @@
+package cursojava.aula43.exercicio;
+
+import java.util.Scanner;
+
+public class ContaBancaria {
+
+    private String nomeCliente;
+    private int numConta;
+    private double saldo;
+
+    public ContaBancaria() {
+        //Construtor vazio
+    }
+
+    public ContaBancaria(String nomeCliente, int numConta, double saldo) {
+        this.nomeCliente = nomeCliente;
+        this.numConta = numConta;
+        this.saldo = saldo;
+    }
+
+    //Métodos get e set
+    public String getNomeCliente() {return nomeCliente;}
+
+    public void setNomeCliente(String nomeCliente) {this.nomeCliente = nomeCliente;}
+
+    public int getNumConta() {return numConta;}
+
+    public void setNumConta(int numConta) {this.numConta = numConta;}
+
+    public double getSaldo() {return saldo;}
+
+    public void setSaldo(double saldo) {this.saldo = saldo;}
+
+
+
+    public double saqar(){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o valor do saque");
+        double valorSaque = scan.nextDouble();
+
+        if (valorSaque > saldo){
+            System.out.println("Saldo insuficiente para sacar. ");
+        } else{
+            System.out.println("Saque realizado com sucesso! ");
+            saldo -= valorSaque;
+        }
+        return valorSaque;
+    }
+
+    public double depositar(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o valor do deposito: ");
+        double valorDeposito = scan.nextDouble();
+
+        if(valorDeposito <= 0){
+            System.out.println("Erro.\nValor incorreto.");
+            return 0;
+        } else{
+            System.out.println("Valor depositado com sucesso! ");
+            saldo += valorDeposito;
+        }
+        return valorDeposito;
+    }
+
+}
