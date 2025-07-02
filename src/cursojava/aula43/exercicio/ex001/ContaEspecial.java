d51215504ff8b906b0f77fe4526bdbcab0a2c5e0
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-07-02 19:34:20 -0300

    Atualização da classe ContaEspecial

---
+++ src/cursojava/aula43/exercicio/ex001/ContaEspecial.java
@@ -0,0 +1,40 @@
+package cursojava.aula43.exercicio.ex001;
+
+public class ContaEspecial extends ContaBancaria {
+
+    private double limite;
+
+    public ContaEspecial() {
+
+    }
+
+    public ContaEspecial(double limite) {
+        this.limite = limite;
+    }
+
+    public double getLimite() {return limite;}
+
+    public void setLimite(double limite) {this.limite = limite;}
+
+    public double sacar(double valorSaque) {
+        double saldo = getSaldo();
+        double limiteDisponivel = getLimite();
+
+        if (valorSaque > (saldo + limiteDisponivel)) {
+            System.out.println("Saldo insuficiente. ");
+
+            return 0;
+        } else {
+            setSaldo(saldo - valorSaque);
+
+            if (valorSaque > saldo){
+                System.out.println("Saque realizado usando o limite. ");
+                
+            } else{
+                System.out.println("Saque realizado com saldo disponível. ");
+            }
+
+        }
+        return valorSaque;
+    }
+}
