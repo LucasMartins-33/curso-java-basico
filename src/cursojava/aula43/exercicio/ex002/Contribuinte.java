14465a760fbaa6947c70369c95bd2ab07c7a951c
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-07-15 14:19:11 -0300

    Classe contribuinte atualizada

--- src/cursojava/aula43/exercicio/ex002/Contribuinte.java
+++ src/cursojava/aula43/exercicio/ex002/Contribuinte.java
@@ -1,4 +1,36 @@
 package cursojava.aula43.exercicio.ex002;
 
 public class Contribuinte {
+
+    private String nome;
+    private double rendaBruta;
+
+    public Contribuinte() {
+
+    }
+
+    public Contribuinte(String nome, double rendaBruta){
+        this.nome = nome;
+        this.rendaBruta = rendaBruta;
+    }
+
+    public String getNome() {return nome;}
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getRendaBruta() {return rendaBruta;}
+
+    public void setRendaBruta(double rendaBruta) {
+        this.rendaBruta = rendaBruta;
+    }
+
+    @Override
+    public String toString() {
+        return "---- Dados do Contribuinte ----\n" +
+                "Nome: " + nome + "\n" +
+                "Renda Bruta: R$ " + String.format("%.2f", rendaBruta);
+    }
+    
 }
