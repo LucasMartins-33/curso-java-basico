b03ec8488c39814d8fa8486f910acabd4ccb52fe
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2025-07-16 12:06:38 -0300

    Adicionar atributos, construtor e métodos get e set

--- src/cursojava/aula43/exercicio/ex003/Peixe.java
+++ src/cursojava/aula43/exercicio/ex003/Peixe.java
@@ -1,4 +1,20 @@
 package cursojava.aula43.exercicio.ex003;
 
 public class Peixe {
+
+    private String caracteristicas;
+
+    public Peixe() {
+
+    }
+
+    public String getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(String caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+
+    
 }
